89c9bcf62c6e01ba38315e6095f2ba509155098c
Alexey 1981 <maximenko.av@rambler.ru>
2023-09-16 22:13:33 +0300

    Second commit

---
+++ src/test/java/ru/netology/Rush61/RestServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.Rush61;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+public class RestServiceTest {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/data.csv")
+    public void shouldRestMounth(int income,int expenses,int threshold,int expected ){
+        RestService service=new RestService();
+        int actual = service.restCalc(income,expenses,threshold);
+        Assertions.assertEquals(expected,actual);
+
+
+    }
+
+}
