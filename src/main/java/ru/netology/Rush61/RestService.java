56c681c0f0a98062e090d9c4376fc2aca97dafc9
Alexey 1981 <maximenko.av@rambler.ru>
2023-09-20 05:45:49 +0300

    change

--- src/main/java/ru/netology/Rush61/RestService.java
+++ src/main/java/ru/netology/Rush61/RestService.java
@@ -4,10 +4,9 @@ public class RestService {
     public int restCalc(int income, int expenses, int threshold) {
         int count = 0;
         int money = 0;
-        int mounth;
-        for (mounth = 1; mounth < 13; mounth++) {
+        for (int mounth = 1; mounth < 13; mounth++) {
             if (money > threshold) {
-                money = money - (expenses * 4);
+                money = (money - expenses) / 4;
                 count++;
             } else {
                 money = money + income - expenses;
